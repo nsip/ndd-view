5da12c2de8cac8adda7403a51d7911173f0956f9
Qing Miao <cdutwhu@outlook.com>
2023-09-08 20:47:33 +1000

    * build.sh for releasing to EC2

--- build.sh
+++ build.sh
@@ -2,34 +2,72 @@
 
 set -e
 
-R=`tput setaf 1`
-G=`tput setaf 2`
-Y=`tput setaf 3`
-W=`tput sgr0`
+#######################################################################################
 
-#####################################
+if [[ $1 == 'test' ]]; then
+    file="./src/share/ip.ts"
+    search="data-dictionary.test"
+    replacement="data-dictionary.link"    
+    sed -i "s#${search}#${replacement}#g" "$file"
+fi
 
-# if [[ -z "$1" ]]
-# then 
-#     echo "${R}need args [domain] or [local], abort. (src/share/ip_***.ts.bak could be modified if needed!)${R}"
-#     exit
-# fi
+if [[ $1 == 'prod' || $1 == 'product' ]]; then
 
-rm -f *.log
+    file="./src/share/ip.ts"
+    
+    search="data-dictionary.test"
+    replacement="data-dictionary.net"    
+    sed -i "s#${search}#${replacement}#g" "$file"
+
+    search="http://"
+    replacement="https://"
+    sed -i "s#${search}#${replacement}#g" "$file"
+
+fi
+
+#######################################################################################
+
 rm -rf ./dist
 
-# change src IP etc.
-
-# if [[ $1 == 'domain' ]]
-# then
-#     cp ./src/share/ip_domain.ts.bak ./src/share/ip.ts
-# fi
-
-# if [[ $1 == 'local' ]]
-# then
-#     cp ./src/share/ip_local.ts.bak ./src/share/ip.ts
-# fi
-
-#####################################
-
 npm run build
+
+#######################################################################################
+
+if [ -f "$2" ]; then
+    if [[ $1 == 'test' ]]; then IP='3.24.210.59'; fi
+    if [[ $1 == 'prod' || $1 == 'product' ]]; then IP='54.66.224.148'; fi
+        
+    TM=`date +%F@%T@%Z`
+    CD=`pwd`
+    PKG_NAME='dist'
+    echo "scp -i $2 -r $CD/$PKG_NAME ubuntu@$IP:dd/$PKG_NAME($TM)"
+    scp -i $2 -r $CD/$PKG_NAME ubuntu@$IP:dd/ndd-view-$PKG_NAME\($TM\)
+
+else
+    echo "valid key file is not provided, cannot send package to EC2"
+fi
+
+#######################################################################################
+
+if [[ $1 == 'test' ]]; then
+    file="./src/share/ip.ts"
+    search="data-dictionary.link"
+    replacement="data-dictionary.test"    
+    sed -i "s#${search}#${replacement}#g" "$file"
+fi
+
+if [[ $1 == 'prod' || $1 == 'product' ]]; then
+
+    file="./src/share/ip.ts"
+    
+    search="data-dictionary.net"
+    replacement="data-dictionary.test"    
+    sed -i "s#${search}#${replacement}#g" "$file"
+
+    search="https://"
+    replacement="http://"
+    sed -i "s#${search}#${replacement}#g" "$file" 
+
+fi
+
+#######################################################################################
