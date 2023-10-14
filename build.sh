960f272a34b83660e6ba7bd8023e36207a3e1549
Qing Miao <cdutwhu@outlook.com>
2023-10-14 19:33:23 +1100

    * build.sh

--- build.sh
+++ build.sh
@@ -78,8 +78,8 @@ if [ -f "$2" ]; then
     TM=`date +%F@%T@%Z`
     CD=`pwd`
     PKG_NAME='dist'
-    echo "scp -i $2 -r $CD/$PKG_NAME ubuntu@$IP:dd/$PKG_NAME($TM)"
-    scp -i $2 -r $CD/$PKG_NAME ubuntu@$IP:dd/ndd-view-$PKG_NAME\($TM\)
+    echo "scp -i $2 -r $CD/$PKG_NAME ubuntu@$IP:dd/$PKG_NAME-$TM"
+    scp -i $2 -r $CD/$PKG_NAME ubuntu@$IP:dd/ndd-view-$PKG_NAME-$TM
 
 else
     echo "${Y}valid key file is not provided, cannot send package to EC2${W}"
